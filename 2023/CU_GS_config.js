41f8eea9232ec5b6617885248cfffceef4b52aab
LeopardCheetah <68766450+LeopardCheetah@users.noreply.github.com>
2023-03-10 20:33:01 -0800

    change to cvr since matches are out

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023caph",
+      "defaultValue": "2023cafr",
       "required": "true",
       "disabled": "true"
     },
