3e3b2099c9f2224a4eec9b5f50bb961501895fbe
LeopardCheetah <68766450+LeopardCheetah@users.noreply.github.com>
2023-04-05 20:58:34 -0700

    change from cvr to svr

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023cafr",
+      "defaultValue": "2023casj",
       "required": "true",
       "disabled": "true"
     },
